1a4bcbe08a4220006a846904f34abb6b1da051f8
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2012-11-16 16:52:37 +0900

    Add default recipe.

---
+++ recipes/default.rb
@@ -0,0 +1,32 @@
+include_recipe "java"
+
+case node["platform_family"]
+when "debian"
+  include_recipe "apt"
+
+  apt_repository "jenkins" do
+    uri "http://pkg.jenkins-ci.org/debian"
+    distribution "binary/"
+    components [""]
+    key "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+    action :add
+  end
+
+when "rhel"
+  include_recipe "yum"
+
+  yum_key "RPM-GPG-KEY-jenkins" do
+    url "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
+    action :add
+  end
+
+  yum_repository "jenkins" do
+    description "Jenkins Official repo"
+    url "http://pkg.jenkins-ci.org/redhat"
+    key "RPM-GPG-KEY-jenkins"
+    action :add
+  end
+end
+
+package "jenkins"
+
